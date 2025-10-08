cc174678390b69250b527a3af94864a6e61641ef
Maria Eduarda Araújo de Moraes <109601939+meaeduarda@users.noreply.github.com>
2025-10-08 11:45:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('#mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
