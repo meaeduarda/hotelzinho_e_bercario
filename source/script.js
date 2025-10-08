2b77907546aca8403b73a1da4454fb2daac51512
Maria Eduarda Araújo de Moraes <109601939+meaeduarda@users.noreply.github.com>
2025-10-08 11:41:48 -0300

    Delete source directory

--- source/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('#mobile-menu');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
